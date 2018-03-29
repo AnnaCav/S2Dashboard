700d5f0efac6123ef7b1ace274a8df9676d56ded
AnnaCav <anna-maria.cavallaro@stud.sbg.ac.at>
2018-03-29 13:10:29 +0200

    Update PopupContent.js

--- scripts/PopupContent.js
+++ scripts/PopupContent.js
@@ -1,3 +1,5 @@
+/*################## Set popup content - Anna-Maria Cavallaro April 2018 #################*/
+
 	var lat;
 	var lon;
 	var latlon;
@@ -7,8 +9,11 @@
 	var timediff
 	var text;
 	var flight;
-
+  
+  /*set popup on clicked position*/
 	function onMapClick(e) {
+		
+			timeout = setTimeout(function(){ alert("Unfortunately the server is loading very slowly. Please try it again."); }, 90000);
 
 			if (typeof(newMarker)==='undefined'){
 				newMarker = new L.marker(e.latlng, { draggable: true });
@@ -35,10 +40,9 @@
 					.setLatLng(latlon)
 					.setContent("Loading...")
 					.openOn(satMap);			
-			
-			
-}
+  }
 
+  /*get next acquisition date*/
 	satMap.on('click', onMapClick);
 	
 				function loadAcquisition() {
@@ -61,7 +65,8 @@
 					}
 				  loadOverflight();
 					}
-					
+				
+        /*get next overflight date*/	
 				function loadOverflight() {
 				var link = "http://sentinel-dashboard.zgis.at/geoserver/s2/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=s2:upcoming_s2_passes&maxFeatures=50&outputFormat=application/json&viewparams=LAT:"+lat+";LON:"+lon;
 				  var xhttp = new XMLHttpRequest();
@@ -75,10 +80,11 @@
 				  }
 				  function ovfFunction(xml){
 				  var satflight = JSON.parse (xml.responseText);
-				  overflight = [satflight.features[1].properties.satellite, satflight.features[0].properties.orbit, satflight.features[0].properties.timestamp_sattime, satflight.features[0].properties.duration_sattime];
+				  overflight = [satflight.features[0].properties.satellite, satflight.features[0].properties.orbit, satflight.features[0].properties.timestamp_sattime, satflight.features[0].properties.duration_sattime];
 				  loadTime();
 					}
-					
+				
+        /*get time difference*/	
 				function loadTime() {
 				var key="61HI0U0RUHOM";
 				var link = "http://api.timezonedb.com/v2/get-time-zone?key="+key+"&format=json&by=position&lat="+lat+"&lng="+lon;
@@ -96,10 +102,10 @@
 				  timediff = parseInt((data.gmtOffset)/3600);
 
 					createText();
-					}
-					
-				function createText(){
+				  }
 				
+        /* create text for content */	
+				function createText(){
 				text="<table style='text-align:center;vertical-align:middle; border-spacing:5px;'><tr><td><b>Satellite</b></td><td><b>Start (UTC)</b></td><td><b>Stop (UTC)</b></td><td><b>Timeliness</b></td><td><b>Mode</b></td></tr><tr>";
 				for (i=0; i<acquisition.length; i++){
 					if([i + 1] % 5 == 0){
@@ -117,15 +123,17 @@
 				
 				flight = "<b>Satellite:</b> "+overflight[0]+"<br><b>Orbit:</b> "+overflight[1]+"<br><b>Time:</b> "+overflight[2]+"<br><b>Duration until overflight (d/h/m/s):</b> "+overflight[3];
 				
-				now()
+				setContent()
 				}
 
-	function now(){
-
-	var popup=L.popup({maxWidth: "auto"});
-	
-	popup
-			.setLatLng(latlon)
-			.setContent("<b>Local time: UTC " + timediff + "h</b><br><br><b>Next acquisitions:</b><br>" + text + "<br><br><b>Next overflight:</b><br>" + flight)
-			.openOn(satMap);
-	}
+        /* display popup with content */
+				function setContent(){
+				var popup=L.popup({maxWidth: "auto"});
+				
+				popup
+						.setLatLng(latlon)
+						.setContent("<b>Local time: UTC " + timediff + "h</b><br><br><b>Next acquisitions:</b><br>" + text + "<br><br><b>Next overflight:</b><br>" + flight)
+						.openOn(satMap);
+						
+				clearTimeout(timeout);
+				}
